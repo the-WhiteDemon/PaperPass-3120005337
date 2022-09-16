303b04012f08cdc528b731f2359aa51bbd929d52
3120005337何智勇 <xxx@qq.com>
2022-09-16 22:48:41 +0800

    main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import jieba
 def get_file_contents(path):
     str = ''
     f = open(path,'r',encoding='UTF-8')   #打开文件
-    line = f.readline(    #读取当前行
+    line = f.readline()   #读取当前行
     while line:
         str = str + line  #拼接文本
         line = f.readline()  #读取下一行
@@ -14,13 +14,52 @@ def get_file_contents(path):
     return str  #返回文本
 
 # 将文本进行jieba分词
-def filter(str):
+def str_filter(str):
     str = jieba.lcut(str) #对文本进行jieba分词
     result = []   #用于存储分词结果
     for tags in str:
-        if(re.match(u"[a-zA-Z0-9\u4e00-u9fa5]",tags)):  #对字母a-z，A-Z，数字0-9，汉字进行保存
+        if(re.match(u"[a-zA-Z0-9\u4e00-\u9fa5]",tags)):  #对字母a-z，A-Z，数字0-9，汉字进行保存
             result.append(tags)
         else:pass
+    return result
+
+#计算余弦相似度
+def calcuate_similarity(text1,text2):
+    #计算文本中每个单词出现的频率
+    texts = [text1,text2]
+    dictionary = gensim.corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    #计算余弦相似度
+    similarity = gensim.similarities.Similarity(None,corpus,num_features=len(dictionary))
+    text_corpus_1 = dictionary.doc2bow(text1)
+    cosine_similarity = similarity[text_corpus_1][1]
+    return cosine_similarity
+
+if __name__ == '__main__':
+    #给出文件的绝对路径
+    original_file = 'E:\软件工程作业\PaperPass-3120005337\作业要求测试文本/orig.txt'
+    add_file = 'E:\软件工程作业\PaperPass-3120005337\作业要求测试文本/orig_0.8_add.txt'
+    answer_file = 'E:\软件工程作业\PaperPass-3120005337/answer.txt'
+    #读取文件
+    original_str = get_file_contents(original_file)
+    add_file = get_file_contents(add_file)
+    #对文本进行jieba分词
+    original_str_filter = str_filter(original_str)
+    add_str_filter = str_filter(add_file)
+    #计算余弦相似度
+    cosine_similarity = calcuate_similarity(original_str_filter,add_str_filter)
+    print("文章相似度：%.8f"%cosine_similarity)
+    #将结果写入answer.txt文件中
+    f = open(answer_file,'w',encoding="UTF-8")
+    f.write("文章相似度：%.8f"%cosine_similarity)
+    f.close()
+    print("文件写入完成")
+
+
+
+
+
+
 
 
 
